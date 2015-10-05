200cdcce837ed2a789855d55e0bbce1ba90d0755
JoeMarion <joeyzmarion@gmail.com>
2015-10-04 23:51:01 -0400

    Add factorial

---
+++ week-4/factorial/my_solution.rb
@@ -0,0 +1,20 @@
+# Factorial
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+def factorial(number)
+  answer = 1
+  i = 0
+  if
+    number == 0
+    return 1
+  else
+    while i < number
+      answer = number * answer
+      number -= 1
+    end
+   end
+  return answer
+end
