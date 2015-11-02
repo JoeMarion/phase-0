d2d8cc2015fad8e8cb8c83bd22e95acc5d3eab6a
JoeMarion <joeyzmarion@gmail.com>
2015-11-01 23:23:42 -0500

    Add game.js

---
+++ week-7/game.js
@@ -0,0 +1,66 @@
+// Design Basic Game Solo Challenge
+
+// This is a solo challenge
+
+// Your mission description: Design a simple three card poker game that will compare the players hand against the dealer.
+// Overall mission: Play against a dealer
+// Goals: Set up a bank that will store the correct balance the player has
+// Characters: Player and dealer
+// Objects: Players hand and the dealers hand
+// Functions: Compare hands value against each other. Function to payout pair plus as well if it was played. Deal function that will randomize the cards and only deal with an appropriate 52 card deck
+
+// Pseudocode
+// Ask the player how much they want to play on the pair plus, and ante.
+// "Deal" the cards to participating players
+// At this point players can not see the dealers hand
+// Ask the player if they would like to continue and play (costing double of what the ante was)
+// Display the dealers hand
+// IF the dealer does not qualify, pay back the play, ELSE compare dealer to player and pay out winner.
+// Analyze players hand and payout pairplus if the player played and won
+
+
+
+// Make a function that will set both the player and dealers hands with a standard deck
+// Variables that are going to be needed are Dealers hand, players hand.
+// Create a function that will compare the poker values of both hands and determine a winner
+
+// Initial Code
+
+var bank = 100;
+var playershand = [];
+var dealershand = [];
+
+function ThreeCard(pairplus, ante) {
+  var playablecards = {
+    heart: [1..13],
+    diamond: [1..13],
+    club: [1..13],
+    spade: [1..13]
+  }
+
+
+  this.hand =
+  this.dealerhand =
+
+
+}
+
+
+
+
+// Refactored Code
+
+
+
+
+
+
+// Reflection
+//
+//
+//
+//
+//
+//
+//
+//
